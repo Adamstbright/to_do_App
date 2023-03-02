6e888ebd2dba245444535614ed6826c9a73551c7
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-02 10:51:21 +0100

    update on the code

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,7 +1,11 @@
 user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
-todos = [todo1, todo2, todo3]
-print(todos)
-print(type(todos))
+
+todos = []
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    todos.append(todo)
+    print(todos)
+
+
+
