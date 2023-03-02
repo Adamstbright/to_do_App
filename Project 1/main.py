55f45c2a36b84a052934eac3ec763372bc0fae07
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-02 22:31:08 +0100

    adding edit to the case

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,8 +1,6 @@
-
-
 todos = []
 while True:
-    user_action  = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,12 +10,13 @@ while True:
         case "show":
             for item in todos:
                 print(item)
+        case "edit":
+            number = int(input("Enter the item to edit: "))
+            number = number - 1
+            new_todo = input("what is the new item to be added: ")
+            todos[number] = new_todo
         case "exit":
             break
         case _:
             print("Kindly input either of the arguments (add, show or exit). Thanks")
 print("bye!")
-
-
-
-
