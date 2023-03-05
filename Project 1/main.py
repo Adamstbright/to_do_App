06bd79c0ba223eca4b2408b729cd7872a18e1ed6
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-05 15:43:48 +0100

    storing data in text file

--- Project 1/main.py
+++ Project 1/main.py
@@ -1,23 +1,37 @@
-todos = []
+
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()               #this will return a list of item in the file as a list of items.
+            file.close()
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)                      # \n is added to the code so that the data going to the text file will not be merged together, thus each item added be on different line in the text file.
+            file.close()
         case "show":
+            file = open('todos.txt', 'r')
+            todos = file.readlines()            #file.readlines always return a list generated from the external text file. file, read will return string if it was used.i.e todos is a list in this case.
+            file.close()
             for index, item in enumerate(todos):
-                print(index,"-", item)
+                row = F"{index + 1}-{item}"
+                print(row)
         case "edit":
             number = int(input("Enter the item to edit: "))
             number = number - 1
             new_todo = input("what is the new item to be added: ")
             todos[number] = new_todo
+        case "complete":
+            number = int(input("Number of item to complete: "))
+            todos.pop(number - 1)
         case "exit":
             break
         case _:
-            print("Kindly input either of the arguments (add, show or exit). Thanks")
+            print("Kindly input either of the arguments (add, show, edit, complete or exit). Thanks")
 print("bye!")
-
