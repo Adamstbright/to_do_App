a44d3a4f4a458ce3a2fbefea840d482d49149335
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-07 20:29:12 +0100

    using if statement to replace match

--- Project 1/main.py
+++ Project 1/main.py
@@ -3,61 +3,60 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ") + "\n"     # \n is added to the code so that the data going to the text file will not be merged together, thus each item added be on different line in the text file.
+    if "add" in user_action:
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()                 # this will return a list of item in the file as a list of items.
+        todo = user_action[4:]
 
-            todos.append(todo)
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()                 # this will return a list of item in the file as a list of items.
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        todos.append(todo)
 
-        case "show":
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()            # file.readlines always return a list generated from the external text file. file, read will return string if it was used.i.e todos is a list in this case.
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            for index, item in enumerate(todos):
-                item = item.strip("\n")             # This is used to remove the \n at added to the todos list, this is neccessary because if not added the list will have unneccsary line brake. you can remove it to see the output.
-                row = F"{index + 1}-{item}"
-                print(row)
-        case "edit":
-            number = int(input("Enter the item to edit: "))
-            number = number - 1
+    elif "show" in user_action:
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()            # file.readlines always return a list generated from the external text file. file, read will return string if it was used.i.e todos is a list in this case.
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        for index, item in enumerate(todos):
+            item = item.strip("\n")             # This is used to remove the \n at added to the todos list, this is neccessary because if not added the list will have unneccsary line brake. you can remove it to see the output.
+            row = F"{index + 1}-{item}"
+            print(row)
+    elif "edit" in user_action:
+        number = int(user_action[5:])
 
-            new_todo = input("what is the new item to be added: ")
-            todos[number] = new_todo + '\n'
+        number = number - 1
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-        case "complete":
-            number = int(input("Number of item to complete: "))
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        new_todo = input("what is the new item to be added: ")
+        todos[number] = new_todo + '\n'
 
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
 
-            todos.pop(index)
+    elif "complete" in user_action:
+        number = int(user_action[9:])
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            with open('todos.txt', 'w') as file:
-                todos = file.writelines(todos)
+        index = number - 1
+        todo_to_remove = todos[index].strip('\n')
 
-            message = f"Todo {todo_to_remove} was removed from the list."
+        todos.pop(index)
 
-        case "exit":
-            break
-        case _:
-            print("Kindly input either of the arguments (add, show, edit, complete or exit). Thanks")
+        with open('todos.txt', 'w') as file:
+            todos = file.writelines(todos)
+
+        message = f"Todo {todo_to_remove} was removed from the list."
+
+    elif "exit" in user_action:
+        break
+    else:
+        print("You have entered a wrong command")
+
 print("bye!")
-
-
-
 
 
