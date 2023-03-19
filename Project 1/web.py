596dd8fc6b10e6186aefa2355fd79f7c9027fd85
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-03-19 22:25:54 +0100

    create delete features

--- Project 1/web.py
+++ Project 1/web.py
@@ -14,11 +14,13 @@ st.title("My todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your productivity")
 
-for a in todos:
-    st.checkbox(a)
-
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
-
-st.session_state
